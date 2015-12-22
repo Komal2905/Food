22b55012e76111b17d2ac99bb79547ad98fe2951
Vidya Ramamurthy <BridgeLabz@vidyas-mac-mini.local>
2015-12-22 18:56:23 +0530

    Initial Commit

---
+++ Food/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Food
+//
+//  Created by Vidya Ramamurthy on 22/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
